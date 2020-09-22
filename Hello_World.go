96bd60e75e566b72a36b1be1f55526c8d3b5fd33
Tomoya Maita <58167405+mt114ran@users.noreply.github.com>
2020-09-22 22:28:38 +0900

    how to use print
    Hello Worldを表示します。

---
+++ Hello_World.go
@@ -0,0 +1,14 @@
+//いまは気にしないでください
+package main
+
+//import宣言でfmtパッケージをインポートします
+//fmtパッケージの中にはPrintln関数などが入っていて使用できるようになります
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World")
+	fmt.Println(2)
+	fmt.Println("2")
+
+}
